265f7b359440f8ba81e73b0678df65df91086c9f
Carlos Mosquera <camosqueraba@gmail.com>
2021-03-29 15:32:37 -0500

    creacion de proyecto

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Encuestas Perfect Body</title>
+    
+    <link rel="stylesheet" id="monsterinsights-popular-posts-style-css" href="../resources/css/frontend.min.css" type="text/css" media="all">
+    <link rel="stylesheet" id="google-font-source-sans-pro-css" href="../resources/css/css" type="text/css" media="all">
+    <link rel="stylesheet" id="google-font-open-sans-css" href="../resources/css/css(1)" type="text/css" media="all">
+    <link rel="stylesheet" id="google-font-pt-serif-css" href="../resources/css/css(2)" type="text/css" media="all">
+    <link rel="stylesheet" id="reset-css" href="./resources/css/reset.css" type="text/css" media="all">
+    <link rel="stylesheet" id="superfish-css" href="../resources/css/superfish.css" type="text/css" media="all">
+    <link rel="stylesheet" id="prettyPhoto-css" href="../resources/css/prettyPhoto.css" type="text/css" media="all">
+    <link rel="stylesheet" id="jquery-qtip-css" href="../resources/css/jquery.qtip.css" type="text/css" media="all">
+    <link rel="stylesheet" id="odometer-css" href="../resources/css/odometer-theme-default.css" type="text/css" media="all">
+    <link rel="stylesheet" id="animations-css" href="../resources/css/animations.css" type="text/css" media="all">
+    <link rel="stylesheet" id="main-style-css" href="../resources/css/style.css" type="text/css" media="all">
+    <link rel="stylesheet" id="responsive-css" href="../resources/css/responsive.css" type="text/css" media="all">
+    <link rel="stylesheet" id="mc-features-css" href="../resources/css/style(1).css" type="text/css" media="all">
+    <link rel="stylesheet" id="mc-template-css" href="../resources/css/style(2).css" type="text/css" media="all">
+    <link rel="stylesheet" id="mc-social-css" href="../resources/css/style(3).css" type="text/css" media="all">
+    <link rel="stylesheet" id="custom-css" href="../resources/css/custom.css" type="text/css" media="all">
+    <link rel="stylesheet" id="joinchat-css" href="../resources/css/joinchat.min.css" type="text/css" media="all">
+    <style id="joinchat-inline-css" type="text/css">
+    .joinchat{ --red:37; --green:211; --blue:102; }
+    </style>
+    <link rel="stylesheet" id="js_composer_front-css" href="../resources/css/js_composer_front_custom.css" type="text/css" media="all">
+    <link rel="stylesheet" id="js_composer_custom_css-css" href="../resources/css/custom(1).css" type="text/css" media="all">
+
+
+</head>
+<body>
+    <div class="site-container fullwidth">
+        <div class="header-top-sidebar-container">
+            <div class="header-top-sidebar clearfix">
+                <div class="textwidget">
+                    <div style='padding: 17px 0;' class='clearfix'>
+                        <ul class='thin-list'>
+                        <li>
+                        <span class="header-icon template-location">Carrera 20 # 15 - 110 Santa Marta</span>
+                        </li>
+                        <li>
+                        <a href="tel:4237101" class="header-icon template-phone">(5) 4237101</a>
+                        </li>
+                        <li>
+                        <a href="mailto:info@perfectbody.com.co" class="header-icon template-mail">info@perfectbody.com.co</a>
+                        </li>
+                        </ul>
+
+                        <div class="icons-list">
+                            <a href="https://twitter.com/perfectbodymb" target="_blank" class="icon-single mc-icon social-twitter" rel="noopener noreferrer"></a>
+                            <a href="https://www.facebook.com/perfectbodymc/" target="_blank" class="icon-single mc-icon social-facebook" rel="noopener noreferrer"></a>
+                            <a href="https://www.instagram.com/perfectbodymc" target="_blank" class="icon-single mc-icon social-instagram" rel="noopener noreferrer"></a>
+
+                            <!--<a href="http://www.perfectbody.com.co/vacantes/">Trabaje con nosotros</a>-->
+                        </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+           
+       <!-- Header -->
+        <div class="header-container ">
+            <div class="header clearfix layout-1">
+                <div class="header-left">
+                    <a href="http://www.perfectbody.com.co" title="Perfect Body Medical Center - Santa Marta Colombia">
+                        <img src="http://www.perfectbody.com.co/wp-content/uploads/2020/05/Perfect-Body-Santa-Marta.png" alt="logo" />
+                    </a>
+                    <a href="#" class="mobile-menu-switch vertical-align-cell">
+                        <span class="line"></span>
+                        <span class="line"></span>
+                        <span class="line"></span>
+                        <span class="line"></span>
+                    </a>
+                </div>
+                <div class="menu-main-menu-container">
+                    <ul id="menu-main-menu" class="sf-menu header-right">
+                        <li id="menu-item-3965" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-3756 current_page_item menu-item-3965">
+                            <a href="http://www.perfectbody.com.co/" aria-current="page">
+                                INICIO
+                            </a>
+                        </li>
+                        <li id="menu-item-3995" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-ancestor current-menu-parent current_page_parent current_page_ancestor menu-item-has-children menu-item-3995">
+                            <a href="http://www.perfectbody.com.co/la-institucion/">
+                                NUESTRA INSTITUCIÓN
+                            </a>
+                            <ul class="sub-menu">
+                                <li id="menu-item-4362" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-has-children menu-item-4362">
+                                    <a href="http://www.perfectbody.com.co/#" aria-current="page">
+                                        Marco legal
+                                    </a>
+                                    <!--
+                                    <ul class="sub-menu">
+                                        <li id="menu-item-4361" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-4361">
+                                            <a href="https://drive.google.com/drive/folders/1k4Fs7dxleOv89UiUd29qqSjPYUMo-2O3?usp=sharing">
+                                                Actas Copasst
+                                            </a>
+                                        </li>
+                                        <li id="menu-item-4363" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-4363">
+                                            <a href="https://drive.google.com/drive/folders/1pEoKvIP8MGGIm8WfdSxF8bTAIg0kQnOE?usp=sharing">
+                                                Balance general y Estado de resultados
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    -->
+                                </li>
+                                <!--
+                                <li id="menu-item-4293" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4293">
+                                    <a href="http://www.perfectbody.com.co/vacantes/">
+                                        Trabaje con nosotros
+                                    </a>
+                                </li>
+                                -->
+                            </ul>
+                        </li>
+                        <li id="menu-item-4071" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4071">
+                            <a href="http://www.perfectbody.com.co/servicios/">
+                                SERVICIOS
+                            </a>
+                        </li>
+                        <li id="menu-item-4001" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4001"><a href="http://www.perfectbody.com.co/informacion-paciente-y-familia/">INFORMACIÓN PACIENTE Y FAMILIA</a>
+                            <ul class="sub-menu">
+                                <li id="menu-item-4261" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4261">
+                                    <a href="http://www.perfectbody.com.co/atencion-al-usuario/">
+                                        Atención al usuario
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li id="menu-item-3939" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3939">
+                            <a href="http://www.perfectbody.com.co/blog/">
+                                NOVEDADES
+                            </a>
+                        </li>
+                        <li id="menu-item-3922" class="left-flyout menu-item menu-item-type-post_type menu-item-object-page menu-item-3922">
+                            <a href="http://www.perfectbody.com.co/contacto/">
+                                CONTACTO
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                
+                
+                <!-- pantalla mobile -->
+                <div class="mobile-menu-container clearfix">
+                    <div class="mobile-menu-divider"></div>
+                    <nav class="mobile-menu collapsible-mobile-submenus">
+                        <ul id="menu-main-menu-1" class="menu">
+                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-3756 current_page_item menu-item-3965">
+                                <a href="http://www.perfectbody.com.co/" aria-current="page">
+                                    INICIO
+                                </a>
+                            </li>
+                            <li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-ancestor current-menu-parent current_page_parent current_page_ancestor menu-item-has-children menu-item-3995"><a href="http://www.perfectbody.com.co/la-institucion/">NUESTRA INSTITUCIÓN</a><a href="#" class="template-arrow-menu"></a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-has-children menu-item-4362"><a href="http://www.perfectbody.com.co/#" aria-current="page">Marco legal</a>
+                                        <ul class="sub-menu">
+                                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-4361">
+                                                <a href="https://drive.google.com/drive/folders/1k4Fs7dxleOv89UiUd29qqSjPYUMo-2O3?usp=sharing">
+                                                    Actas Copasst
+                                                </a>
+                                            </li>
+                                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-4363">
+                                                <a href="https://drive.google.com/drive/folders/1pEoKvIP8MGGIm8WfdSxF8bTAIg0kQnOE?usp=sharing">
+                                                    Balance general y Estado de resultados
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4293">
+                                        <a href="http://www.perfectbody.com.co/vacantes/">
+                                            Trabaje con nosotros
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4071">
+                                <a href="http://www.perfectbody.com.co/servicios/">
+                                    SERVICIOS
+                                </a>
+                            </li>
+                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4001">
+                                <a href="http://www.perfectbody.com.co/informacion-paciente-y-familia/">
+                                    INFORMACIÓN PACIENTE Y FAMILIA
+                                </a>
+                                <a href="#" class="template-arrow-menu"></a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4261">
+                                        <a href="http://www.perfectbody.com.co/atencion-al-usuario/">
+                                            Atención al usuario
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3939">
+                                <a href="http://www.perfectbody.com.co/blog/">
+                                    NOVEDADES
+                                </a>
+                            </li>
+                            <li class="left-flyout menu-item menu-item-type-post_type menu-item-object-page menu-item-3922">
+                                <a href="http://www.perfectbody.com.co/contacto/">
+                                    CONTACTO
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>    
+                </div>    							
+            </div>
+        </div>
+    </div>
+
+
+
+    <script type='text/javascript' src='../resources/js/jquery/ui/core.min.js?ver=1.11.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery/ui/widget.min.js?ver=1.11.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery/ui/accordion.min.js?ver=1.11.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery/ui/tabs.min.js?ver=1.11.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery/ui/datepicker.min.js?ver=1.11.4'></script>
+    <script type='text/javascript'>
+    jQuery(document).ready(function(jQuery){jQuery.datepicker.setDefaults({"closeText":"Close","currentText":"Today","monthNames":["January","February","March","April","May","June","July","August","September","October","November","December"],"monthNamesShort":["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],"nextText":"Next","prevText":"Previous","dayNames":["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],"dayNamesShort":["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],"dayNamesMin":["S","M","T","W","T","F","S"],"dateFormat":"MM d, yy","firstDay":1,"isRTL":false});});
+    </script>
+    <script type='text/javascript' src='../resources/js/jquery.imagesloaded-packed.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery.ba-bbq.min.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery.history.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery.easing.1.3.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery.carouFredSel-6.2.1-packed.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery.sliderControl.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery.timeago.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery.hint.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery.isotope-packed.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery.prettyPhoto.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery.qtip.min.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery.blockUI.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/jquery.parallax.min.js?ver=5.4.4'></script>
+    <script type='text/javascript'>
+    /* <![CDATA[ */
+    var config = [];
+    config = {"ajaxurl":"http:\/\/www.perfectbody.com.co\/wp-admin\/admin-ajax.php","themename":"medicenter","home_url":"http:\/\/www.perfectbody.com.co","is_rtl":0};;
+    /* ]]> */
+    </script>
+    <script type='text/javascript' src='../resources/js/main.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/odometer.min.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/plugins/creame-whatsapp-me/public/js/joinchat.min.js?ver=4.1.10'></script>
+    <script type='text/javascript' src='../resources/js//js/wp-embed.min.js?ver=5.4.4'></script>
+    <script type='text/javascript' src='../resources/js/plugins/js_composer/assets/js/dist/js_composer_front.min.js?ver=6.1'></script>
+</body>
+</html>
