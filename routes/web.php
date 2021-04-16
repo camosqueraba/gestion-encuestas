baddc1e2a25a5c50f6ae8420d53d1d2bee46a3fe
Carlos Mosquera <camosqueraba@gmail.com>
2021-04-16 14:06:05 -0500

    agregada vista y ruta principal

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('perfectbody');
 });
+
+Route::get('/principal', function () {
+    return view('principal');
+});
